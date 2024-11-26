d9e7eb7d1af4121976a3f8a76a4f0f173ac4f55c
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-26 11:15:25 -0600

    Update Evolución del salario minimo.py

--- pages/Evolución del salario minimo.py
+++ pages/Evolución del salario minimo.py
@@ -9,6 +9,24 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # Datos ficticios de evolución del salario mínimo
 data = {
     "Año": list(range(2000, 2025)),
