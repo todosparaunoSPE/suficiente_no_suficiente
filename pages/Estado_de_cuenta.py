5a8f297a635e5a1b5581aab31c477e8106f2d367
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-26 11:15:03 -0600

    Update Estado_de_cuenta.py

--- pages/Estado_de_cuenta.py
+++ pages/Estado_de_cuenta.py
@@ -11,6 +11,22 @@ import pandas as pd
 import streamlit as st
 import pandas as pd
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Title
 st.title("Estado de Cuenta: PENSIONISSSTE")
 st.write("**Trabajador:** Javier Horacio Pérez Ricárdez")
